6d84100aa3ff6f490310e99e3064a4dbfc4411f6
Dawson Nethery <dawsonnethery@Dawsons-MacBook-Pro.local>
2020-11-04 18:15:29 -0600

    configuring for heroku

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
-# django_pages/urls.py
-from django.contrib import admin
-from django.urls import path, include # new
+# pages/urls.py
+from django.urls import path
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')), # new
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', HomePageView.as_view(), name='home'),
 ]
